9ac31722f6cc7f8df7812fad6c3021d874484c0a
Abhishek Anand <64414279+MildAbhishek@users.noreply.github.com>
2024-09-22 06:31:39 +0000

    functions in ts

--- 02BASICS/functions.js
+++ 02BASICS/functions.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function add(num) {
     // num.toUpperCase() // error
-    return num + 2;
+    return num + 5;
+    return 'OK'; // no error
 }
 add(5);
 // add('5') // error
@@ -19,6 +20,28 @@ function loginUser(name, email, isPaid) {
     if (isPaid === void 0) { isPaid = true; }
     return 'Login User';
 }
-loginUser('Abhishek', 'xyz@test.com');
-function getMilege() {
+loginUser('Abhishek', 'xyz@test.com'); // 3rd parameter will be taken by default as true
+// return type is not explicit mentioned, so no error
+function getMilege(milege) {
+    if (milege > 35) {
+        return true;
+    }
+    return '0K';
+}
+// syntax for arrow function
+var calculate = function (s) {
+    return 'Calculated';
+};
+var cars = ['BMW', 'Mercedes', 'Skoda'];
+cars.map(function (car) {
+    return "car is ".concat(car);
+    // return 1 // error
+});
+// not returning anything
+function consoleError(errMsg) {
+    console.log(errMsg);
+}
+// intentional crash, handling exception - return type should be never as per official documentation
+function handleError(errMsg) {
+    throw new Error(errMsg);
 }
--- 02BASICS/functions.ts
+++ 02BASICS/functions.ts
@@ -1,6 +1,8 @@
 function add(num: number){
     // num.toUpperCase() // error
-    return num + 2
+    return num + 5
+
+    return 'OK' // no error
 }
 
 add(5)
@@ -21,9 +23,36 @@ signUp('Abhishek', 'xyz@test.com', true)
 function loginUser(name: string, email: string, isPaid: boolean = true){
     return 'Login User'
 }
-loginUser('Abhishek', 'xyz@test.com')
+loginUser('Abhishek', 'xyz@test.com') // 3rd parameter will be taken by default as true
 
-function getMilege(){
-    
+
+// return type is not explicit mentioned, so no error
+function getMilege(milege: number){
+    if(milege > 35){
+        return true
+    }
+   return '0K'
 }
+
+// syntax for arrow function
+const calculate = (s: string): string => {
+    return 'Calculated'
+}
+
+const cars = ['BMW', 'Mercedes', 'Skoda']
+cars.map((car: string): string=>{
+    return `car is ${car}`
+    // return 1 // error
+})
+
+// not returning anything
+function consoleError(errMsg: string): void{
+    console.log(errMsg)
+}
+
+// intentional crash, handling exception - return type should be never as per official documentation
+function handleError(errMsg: string): never{
+   throw new Error(errMsg)
+}
+
 export {}
