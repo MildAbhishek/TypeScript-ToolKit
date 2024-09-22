03b5f98bba4bfe9a59097dab3afcd7a1bd23dab0
Abhishek Anand <64414279+MildAbhishek@users.noreply.github.com>
2024-09-22 13:30:18 +0000

    enum

---
+++ 02BASICS/enums.js
@@ -0,0 +1,18 @@
+var RoomNo;
+(function (RoomNo) {
+    RoomNo[RoomNo["AISLE"] = 1001] = "AISLE";
+    RoomNo[RoomNo["MIDDLE"] = 1002] = "MIDDLE";
+    RoomNo[RoomNo["WINDOW"] = 1003] = "WINDOW";
+})(RoomNo || (RoomNo = {}));
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice["MIDDLE"] = "middle";
+    SeatChoice["WINDOW"] = "window";
+})(SeatChoice || (SeatChoice = {}));
+var SeatChoices;
+(function (SeatChoices) {
+    SeatChoices["AISLE"] = "aisle";
+    SeatChoices[SeatChoices["MIDDLE"] = 1002] = "MIDDLE";
+    SeatChoices[SeatChoices["WINDOW"] = 1003] = "WINDOW";
+})(SeatChoices || (SeatChoices = {}));
---
+++ 02BASICS/enums.ts
@@ -0,0 +1,17 @@
+enum RoomNo {
+    AISLE = 1001,
+    MIDDLE, // 1002
+    WINDOW, // 1003
+}
+
+enum SeatChoice {
+    AISLE = 'aisle',
+    MIDDLE = 'middle',
+    WINDOW = 'window',
+}
+
+enum SeatChoices {
+    AISLE = 'aisle',
+    MIDDLE = 1002,
+    WINDOW, // 1003
+}
